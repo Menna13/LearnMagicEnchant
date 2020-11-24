f597d386ab35f08698062dddcf86b456c8051596
Menna Khaliel <56710050+Menna13@users.noreply.github.com>
2020-11-24 00:48:23 -0500

    some final edits

--- app/src/main/java/com/example/learnmagicenchant/MainActivity.java
+++ app/src/main/java/com/example/learnmagicenchant/MainActivity.java
@@ -2,29 +2,43 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
     public class MainActivity extends AppCompatActivity {
+        //dictonary for latin > english word definitions
         HashMap<String, String> dict = new HashMap<String, String>();
-        Button btnChant;
+        EditText edInput;
+        Button btnLook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //change title bar
+        getSupportActionBar().setTitle("The Power of your Spell~");
         setContentView(R.layout.activity_main);
-        btnChant = findViewById(R.id.btnChant);
-        btnChant.setOnClickListener(new View.OnClickListener() {
+
+        //get layout components
+        edInput = findViewById(R.id.edInput);
+        btnLook = findViewById(R.id.btnLook);
+
+        //set click lister for the button to initiate decipher process
+        btnLook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //run the function responsible for loading the dictionary
                 readDictionary();
 
             }
@@ -32,25 +46,34 @@ import java.util.HashMap;
 
     }
 
+    //readDictionary() loads the latin > english dictionary and add each latin word and english definition as key-value pairs in a hashmap
     private void readDictionary() {
-//        System.out.println("got here");
+        //loading dictionary csv file from resources
         InputStream is = getResources().openRawResource(R.raw.latino);
         BufferedReader reader = new BufferedReader(
-                new InputStreamReader(is, Charset.forName("UTF-8"))
+                new InputStreamReader(is, Charset.forName("windows-1255"))
         );
-
         String line = "";
-        int counter = 1;
         {
             try {
                 while(((line = reader.readLine()) != null)){
-                    //Split by
-                    counter++;
-                    String [] tokens = line.split(",");
-//                    System.out.println("maybe anything?");
-                    System.out.println(tokens[counter]);
+                    String [] def = line.split("\\$");
 
+                    //adding first index as key (latin word), adding second index as value (english definition)
+                    //trim space ending of latin word (decided after debugging)
+                    //substring definition of unnecessary symbols (decided after debugging)
+                    dict.put(def[0].trim(), def[1].substring(3, def[1].length()-2));
                 }
+
+                //set up intent to send information to the new activity readSpell (translation screen)
+                Intent i = new Intent(this, readSpell.class);
+
+                //send data: dictionary hashmap and the input string for latin word (spell)
+                i.putExtra("dictionary", dict);
+                i.putExtra("spell", edInput.getText().toString());
+
+                //start the activity with the data
+                startActivity(i);
 
             } catch (IOException e) {
                 Log.wtf("myActivity", "Error reading data file" + line, e);
---
+++ app/src/main/java/com/example/learnmagicenchant/readSpell.java
@@ -0,0 +1,45 @@
+package com.example.learnmagicenchant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+public class readSpell extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView tvMeaning;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_spell);
+        getSupportActionBar().setTitle("The Power of your Spell~");
+
+        //get the passed intent to read the data
+        Intent i = getIntent();
+
+        //read data
+        HashMap <String, String> dict = (HashMap<String, String>)i.getSerializableExtra("dictionary");
+        String spell = i.getStringExtra("spell");
+
+        //get layout TextView component
+        tvMeaning = findViewById(R.id.tvMeaning);
+
+        //check if the inout word is in dictionary. If so, print the value in the TextView
+        if (dict.containsKey(spell)){
+            tvMeaning.setText("You Spell can give you "+ dict.get(spell) + " relating powers!");
+        }
+
+        //check if input word is empty string, if so, print a message in the TextView
+        else if (spell.length()==0){
+            tvMeaning.setText("Your Spell is Empty!");
+        }
+        //otherwise, input word is not found, print a message in the TextView
+        else{
+            tvMeaning.setText("Couldn't Decipher your Spell!");
+
+        }
+    }
+}
